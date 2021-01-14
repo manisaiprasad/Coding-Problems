89bcc966d9d1477bdb01f06dfff9e347cc6b0664
manisaiprasadam <manisaiprasadam@gmail.com>
2021-01-15 00:14:59 +0530

    InsertionSort added

---
+++ AlgoExpert/insertionSort.py
@@ -0,0 +1,12 @@
+def insertionSort(array):
+    # Write your code here.
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j] < array[j-1]:
+            swap(j, j-1, array)
+            j -= 1
+    return array
+
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
