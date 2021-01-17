4d3c6a8fb7f0e975ef8e4918ebb2d0b9090c63b0
manisaiprasadam <manisaiprasadam@gmail.com>
2021-01-18 04:46:02 +0530

    linkedListPalindrome Added

---
+++ AlgoExpert/linkedListPalindrome.py
@@ -0,0 +1,35 @@
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def linkedListPalindrome(head):
+    # Write your code here.
+    slow = head
+    fast = head
+    while fast is not None and fast.next is not None:
+        fast = fast.next.next
+        slow = slow.next
+
+    reversedListNode = reverseList(slow)
+    firstHalfNode = head
+
+    while reversedListNode is not None:
+        if reversedListNode.value != firstHalfNode.value:
+            return False
+        reversedListNode = reversedListNode.next
+        firstHalfNode = firstHalfNode.next
+
+    return True
+
+
+def reverseList(head):
+    prevNode, currentNode = None, head
+    while currentNode is not None:
+        nextNode = currentNode.next
+        currentNode.next = prevNode
+        prevNode = currentNode
+        currentNode = nextNode
+    return prevNode
