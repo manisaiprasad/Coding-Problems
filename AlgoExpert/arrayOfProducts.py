909fb53fdef5e20422f582e1068fbaeed106f159
manisaiprasadam <manisaiprasadam@gmail.com>
2021-02-01 12:50:55 +0530

    arrayOfProducts added

---
+++ AlgoExpert/arrayOfProducts.py
@@ -0,0 +1,13 @@
+def arrayOfProducts(array):
+    # Write your code here.
+	products = []
+	product = 1
+    for i in range(len(array)):
+		for j in range(len(array)):
+			if i ==j:
+				continue
+			product *= array[j]
+			print(product)
+		products.append(product)
+		product = 1
+	return products 
